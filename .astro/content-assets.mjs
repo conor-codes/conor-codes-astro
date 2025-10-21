261fefe4bcde5382904be2b2aff5b6902d3dc78a
DESKTOP-SO9CMK4\Conor <conor_a_murphy@outlook.com>
2025-10-21 12:25:37 +0100

    Initial release - Code-themed developer portfolio
    - C# syntax highlighting in About section
    - Project showcase with hover effects
    - Responsive design with dark mode
    - Built with Astro + Tailwind CSS

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_eUzbI from "./meta.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fcode-example%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_N9mps from "./meta.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fexample-post%2Findex.mdx";
+export default new Map([["./meta.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fcode-example%2Findex.mdx", __ASTRO_IMAGE_IMPORT_eUzbI], ["./meta.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fexample-post%2Findex.mdx", __ASTRO_IMAGE_IMPORT_N9mps]]);
+		
