3ee10ce5e7b0259425a706f071e89566ea5fa907
DESKTOP-SO9CMK4\Conor <conorcodes@outlook.com>
2025-10-28 13:09:50 +0000

    fix rename Images to images for case-sensitive deployment

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -5,7 +5,7 @@ export default new Map([
 ["src/content/pages/404.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2F404.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/homepage/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fhomepage%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/writing/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fwriting%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/posts/example-post/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexample-post%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/jobs/reboot-studio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fjobs%2Freboot-studio.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/code-example/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcode-example%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/jobs/reboot-studio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fjobs%2Freboot-studio.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/example-post/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexample-post%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
